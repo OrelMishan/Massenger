91ca9cc0f3c0fbb1e25a6327b4a6b2640e5cf94e
dmmon <dmmonheit@gmail.com>
2022-04-05 14:03:33 +0300

    left menu

--- src/LeftMenu.js
+++ src/LeftMenu.js
@@ -1,13 +1,106 @@
 function LeftMenu() {
-    return(
+    return (
         <div className="left-menu">
-hello
-ffff
-            fff
-            <br/>
-            ff
+
+            <div className="list-group list-group-flush border-bottom scrollarea">
+
+                <nav className="navbar navbar-light bg-light">
+                    <form className="form-inline">
+                        <input className="mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+                            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </nav>
+
+                <a href="#" className="list-group-item list-group-item-action py-3 lh-tight">
+                    <div className="d-flex w-100 align-items-center justify-content-between">
+                        <strong className="mb-1">Name</strong>
+                        <small className="text-muted">side item</small>
+                    </div>
+                </a>
+                <a href="#" className="list-group-item list-group-item-action py-3 lh-tight">
+                    <div className="d-flex w-100 align-items-center justify-content-between">
+                        <strong className="mb-1">Name</strong>
+                        <small className="text-muted">side item</small>
+                    </div>
+                </a>
+                <a href="#" className="list-group-item list-group-item-action py-3 lh-tight">
+                    <div className="d-flex w-100 align-items-center justify-content-between">
+                        <strong className="mb-1">Name</strong>
+                        <small className="text-muted">side item</small>
+                    </div>
+                </a>
+                <a href="#" className="list-group-item list-group-item-action py-3 lh-tight">
+                    <div className="d-flex w-100 align-items-center justify-content-between">
+                        <strong className="mb-1">Name</strong>
+                        <small className="text-muted">side item</small>
+                    </div>
+                </a>
+                <a href="#" className="list-group-item list-group-item-action py-3 lh-tight">
+                    <div className="d-flex w-100 align-items-center justify-content-between">
+                        <strong className="mb-1">Name</strong>
+                        <small className="text-muted">side item</small>
+                    </div>
+                </a>
+                <a href="#" className="list-group-item list-group-item-action py-3 lh-tight">
+                    <div className="d-flex w-100 align-items-center justify-content-between">
+                        <strong className="mb-1">Name</strong>
+                        <small className="text-muted">side item</small>
+                    </div>
+                </a>
+                <a href="#" className="list-group-item list-group-item-action py-3 lh-tight">
+                    <div className="d-flex w-100 align-items-center justify-content-between">
+                        <strong className="mb-1">Name</strong>
+                        <small className="text-muted">side item</small>
+                    </div>
+                </a>
+                <a href="#" className="list-group-item list-group-item-action py-3 lh-tight">
+                    <div className="d-flex w-100 align-items-center justify-content-between">
+                        <strong className="mb-1">Name</strong>
+                        <small className="text-muted">side item</small>
+                    </div>
+                </a>
+                <a href="#" className="list-group-item list-group-item-action py-3 lh-tight">
+                    <div className="d-flex w-100 align-items-center justify-content-between">
+                        <strong className="mb-1">Name</strong>
+                        <small className="text-muted">side item</small>
+                    </div>
+                </a>
+                <a href="#" className="list-group-item list-group-item-action py-3 lh-tight">
+                    <div className="d-flex w-100 align-items-center justify-content-between">
+                        <strong className="mb-1">Name</strong>
+                        <small className="text-muted">side item</small>
+                    </div>
+                </a>
+                <a href="#" className="list-group-item list-group-item-action py-3 lh-tight">
+                    <div className="d-flex w-100 align-items-center justify-content-between">
+                        <strong className="mb-1">Name</strong>
+                        <small className="text-muted">side item</small>
+                    </div>
+                </a>
+                <a href="#" className="list-group-item list-group-item-action py-3 lh-tight">
+                    <div className="d-flex w-100 align-items-center justify-content-between">
+                        <strong className="mb-1">Name</strong>
+                        <small className="text-muted">side item</small>
+                    </div>
+                </a>
+            </div>
+
 
         </div>
-    )
+
+)
 }
 export default LeftMenu
+
+
+
+
+
+
+
+
+
+
+
+
+
