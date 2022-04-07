fa7d75fb676af4b6ea29f043c631e039d04e6967
אוראל משען <orelmishan1@gmail.com>
2022-04-07 10:57:07 +0300

    update chat

--- src/ChatInput.js
+++ src/ChatInput.js
@@ -1,9 +1,8 @@
 function ChatInput() {
     return (
-        <div className="input-group mb-3 send-sec send-input">
-            <input type="text" className="form-control" placeholder="massage"
-                   aria-label="Recipient's username" aria-describedby="button-addon2"/>
-            <button className="btn btn-success left-align " type="button">
+        <div className="input-group mb-3 send-sec">
+            <input type="text"  className="send-input" placeholder="massage"/>
+            <button className="btn btn-success right-align" type="button" >
                 <img src={require("./send.png")} alt="send" height="20px"/>
             </button>
         </div>
