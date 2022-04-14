6c9a0d9da97c6b22f8fb5973ae23f636a80851d4
dmmon <dmmonheit@gmail.com>
2022-04-15 01:51:44 +0300

    fixed contact prev

--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,17 @@
+import Message from "./message";
+
 function Contact({contact, setContact, setListMessage}) {
+    const lastMessage = (message) => {
+        if(message.type === "image") {
+            return  <img src={message.value} className="last-message-image" alt="not found"/>
+        }else if(message.type === "video"){
+
+        }
+        else if(message.type === "record"){
+
+        } else if(message.type === "text"){
+            return <p className="mb-1 message-peak-text">{message.value}</p>        }
+    }
 
     return (
         <a className="list-group-item" id="transparent-bubble" onClick={()=> {
@@ -8,10 +21,13 @@ function Contact({contact, setContact, setListMessage}) {
             <div className="d-flex justify-content-between">
                 <img className="rounded-circle contact-pic"
                      src={"face.jpg"}/>
-                <h5 className="mb-1 big-text text-center">{contact.contactName}</h5>
-                <small className="text-muted date-text">"14/4/2022"</small>
+
+
             </div>
-            <p className="mb-1 message-peak-text">{contact.messages[contact.messages.length-1].value}</p>
+            <h5 className="mb-1 big-text user-name">{contact.contactName}</h5>
+            <small className="text-muted date-text">"14/4/2022"</small>
+            {lastMessage(contact.messages[contact.messages.length-1])}
+            {/*<p className="mb-1 message-peak-text">{contact.messages[contact.messages.length-1].value}</p>*/}
         </a>
     )
 }
