be9856c7ce3321324113ec157e091c4082b2e6cc
אוראל משען <orelmishan1@gmail.com>
2022-04-16 22:24:07 +0300

    scroll to bottom

--- src/chatBox.js
+++ src/chatBox.js
@@ -6,6 +6,7 @@ function ChatBox({listMessages}) {
     let window = useRef(null);
     let dataList = "";
     if (listMessages.length !== 0) {
+        // eslint-disable-next-line array-callback-return
         dataList = listMessages.map((message, key) => {
                 if (message.value !== "")
                     return <Message sender={message.sender} type={message.type} value={message.value} key={key}/>;
